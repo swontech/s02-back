5581d5b9c22128292f8bf575e0b45769278e26dd
minseonghyeon <minseonghyeon@naver.com>
2022-08-14 16:22:56 +0900

    LogAspect 수정

--- app/src/main/java/com/swontech/s02/aop/LogAspect.java
+++ app/src/main/java/com/swontech/s02/aop/LogAspect.java
@@ -1,17 +1,17 @@
 package com.swontech.s02.aop;
 
+import com.google.common.base.Joiner;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -20,31 +20,35 @@ import java.util.stream.Collectors;
 public class LogAspect {
     private static final Logger logger = LoggerFactory.getLogger(LogAspect.class);
 
+    @Pointcut("within(com.swontech.s02.client.controller..*.*)")
+    public void onRequest() {}
 
-//    @Around("within(com.swontech.s02.client..controller..*.*)")
-//    public Object printLog(ProceedingJoinPoint joinPoint) throws Throwable {
-//        return null;
-//    }
-
-    // get requset value
-    private String getRequestParams() {
-        RequestAttributes requestAttribute = RequestContextHolder.getRequestAttributes();
-
-        if (requestAttribute != null) {
-            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
-                    .getRequestAttributes()).getRequest();
-
-            Map<String, String[]> params = request.getParameterMap();
-
+    @Around("com.swontech.s02.aop.LogAspect.onRequest()")
+    public Object doLogging(ProceedingJoinPoint pjp) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        Map<String, String[]> paramMap = request.getParameterMap();
+        String params = "";
+        if(paramMap.isEmpty() == false) {
+            params = "[" + paramMapToString(paramMap) + "]";
         }
-        return null;
-    }
-//
-//    private String paramMapToString(Map<String, String[]> paramMap) {
-//        return paramMap.entrySet().stream()
-//                .map(entry -> String.format("%s -> (%s)",
-//                        entry.getKey(), Joiner.on(",").join(entry.getValue())))
-//                .collect(Collectors.joining(", "));
-//    }
 
+        long start = System.currentTimeMillis();
+        try {
+            return pjp.proceed(pjp.getArgs());
+        } finally {
+            long end = System.currentTimeMillis();
+            logger.info("**************************************************************************************************************");
+            logger.info("* Request Method     : {}", request.getMethod());
+            logger.info("* Request URI        : {}", request.getRequestURI());
+            logger.info("* Request Params     : {}", params);
+            logger.info("* Time Taken         : {}ms", end - start);
+            logger.info("**************************************************************************************************************");
+        }
+    }
+
+    private String paramMapToString(Map<String, String[]> paramMap) {
+        return paramMap.entrySet().stream()
+                .map(entry -> String.format("%s: %s", entry.getKey(), Joiner.on(",").join(entry.getValue())))
+                .collect(Collectors.joining(", "));
+    }
 }

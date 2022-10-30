02055678e1b687eaa2c57587d9dc9de8b47086ed
minseonghyeon <minseonghyeon@naver.com>
2022-10-30 12:03:30 +0900

    수정

--- client/src/main/java/com/swontech/s02/client/service/s021/S021200010Service.java
+++ client/src/main/java/com/swontech/s02/client/service/s021/S021200010Service.java
@@ -6,11 +6,12 @@ import com.swontech.s02.domain.common.security.JwtTokenProvider;
 import com.swontech.s02.domain.store.comm.AuthRedisStore;
 import com.swontech.s02.domain.store.s021.S021200010Store;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class S021200010Service extends S021200010Logic {
-    public S021200010Service(S021200010Store s021200010Store, AuthenticationManagerBuilder authenticationManagerBuilder, JwtTokenProvider jwtTokenProvider, AuthRedisStore authRedisStore, CustomResponse responseDto) {
-        super(s021200010Store, authenticationManagerBuilder, jwtTokenProvider, authRedisStore, responseDto);
+    public S021200010Service(S021200010Store s021200010Store, AuthenticationManagerBuilder authenticationManagerBuilder, JwtTokenProvider jwtTokenProvider, AuthRedisStore authRedisStore, CustomResponse responseDto, PasswordEncoder passwordEncoder) {
+        super(s021200010Store, authenticationManagerBuilder, jwtTokenProvider, authRedisStore, responseDto, passwordEncoder);
     }
 }

08d462b43123cd51fad8000f4a3ed4015600f263
JeongYeongBin <dudqls739@naver.com>
2023-05-25 12:39:30 +0900

    푸시알람 수정

--- app/src/main/java/com/swontech/s02/config/FirebaseConfigration.java
+++ app/src/main/java/com/swontech/s02/config/FirebaseConfigration.java
@@ -3,8 +3,6 @@ package com.swontech.s02.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +10,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 
 @Configuration
@@ -34,20 +30,11 @@ public class FirebaseConfigration {
 //            FileInputStream serviceAccount = new FileInputStream(resource.getFile());
             // Server
             InputStream serviceAccount = new ClassPathResource(filePath).getInputStream();
-//            File tempFile = File.createTempFile("service-account", ".json");
-//            try {
-//                FileUtils.copyInputStreamToFile(inputStream, tempFile);
-//            } finally {
-//                IOUtils.closeQuietly(inputStream);
-//            }
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-//            FirebaseOptions options = new FirebaseOptions.Builder()
-//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
 //                    .setDatabaseUrl("https://byapp-35c1e.firebaseio.com")
-//                    .build();
+                    .build();
             if (FirebaseApp.getApps().isEmpty()) {
                 System.out.println("Firebase Initialize");
                 FirebaseApp.initializeApp(options);

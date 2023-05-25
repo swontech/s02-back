673facf829ca6603c3cc3c87d33eef52523ccf99
JeongYeongBin <dudqls739@naver.com>
2023-05-25 12:32:25 +0900

    푸시알람 수정

--- app/src/main/java/com/swontech/s02/config/FirebaseConfigration.java
+++ app/src/main/java/com/swontech/s02/config/FirebaseConfigration.java
@@ -3,26 +3,44 @@ package com.swontech.s02.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfigration {
 
     @Value("classpath:keystore/service-account.json")
     private Resource resource;
+    private String filePath;
 
     @Bean
     @Primary
     public void initFirebase() {
+        filePath = "keystore/service-account.json";
         try {
             // Service Account를 이용하여 Fireabse Admin SDK 초기화
-            FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+
+            // Local
+//            FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+            // Server
+            InputStream serviceAccount = new ClassPathResource(filePath).getInputStream();
+//            File tempFile = File.createTempFile("service-account", ".json");
+//            try {
+//                FileUtils.copyInputStreamToFile(inputStream, tempFile);
+//            } finally {
+//                IOUtils.closeQuietly(inputStream);
+//            }
+
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();

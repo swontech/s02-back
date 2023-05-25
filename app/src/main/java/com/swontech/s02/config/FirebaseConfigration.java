1830f39cd4458959b91d1fa8ea6323b5cf74dcfc
JeongYeongBin <dudqls739@naver.com>
2023-05-25 11:44:32 +0900

    푸시알람 수정

--- app/src/main/java/com/swontech/s02/config/FirebaseConfigration.java
+++ app/src/main/java/com/swontech/s02/config/FirebaseConfigration.java
@@ -4,10 +4,11 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.Resource;
 
-import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 
 @Configuration
@@ -16,16 +17,21 @@ public class FirebaseConfigration {
     @Value("classpath:keystore/service-account.json")
     private Resource resource;
 
-    @PostConstruct
+    @Bean
+    @Primary
     public void initFirebase() {
         try {
             // Service Account를 이용하여 Fireabse Admin SDK 초기화
             FileInputStream serviceAccount = new FileInputStream(resource.getFile());
-            FirebaseOptions options = new FirebaseOptions.Builder()
+            FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                    .setDatabaseUrl("https://byapp-35c1e.firebaseio.com")
                     .build();
+//            FirebaseOptions options = new FirebaseOptions.Builder()
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    .setDatabaseUrl("https://byapp-35c1e.firebaseio.com")
+//                    .build();
             if (FirebaseApp.getApps().isEmpty()) {
+                System.out.println("Firebase Initialize");
                 FirebaseApp.initializeApp(options);
             }
         } catch (Exception e) {
